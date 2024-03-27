708d5a72fc367abc2c966c2d8f0cd7e6fcb2aabf
Arif Babayev <arifrb@code.edu.az>
2024-03-27 23:05:55 +0400

    birthdayGift

---
+++ config.js
@@ -0,0 +1,33 @@
+var config = {
+
+    texts: [
+        "Doğum günü kızı",     
+        "Səni tanıdığımız", 
+        "üçün çox şanslıyıq",
+        "Yeni yaşında",
+        "daha çox uğurların,",
+        "nailiyyətin və xoşbəxtliklərin",
+        "sənin olması diləyi ilə",
+        "Ad Günün Mübarək!",
+        "Yaxşı ki varsan",
+        "Və çox dəyərlisən",
+        ":)",
+    ],
+   
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        //"今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Işıqları Yandır",
+        play: "Musiqi",
+        bannar_coming: "Başlat",
+        balloons_flying: "İndi isə Şarlar",
+        cake_fadein: "Və Tortunda gəlsin",
+        light_candle: "Şamı yandır",
+        wish_message: "Davam et",
+        story: "Sənə mesaj",
+        cake_cut: "Test"
+    }
+};
